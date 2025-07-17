bf97105b2874ef373eed9995413765f87c1d1dda
McAdams R.D. <93725119+mcthekreator@users.noreply.github.com>
2025-07-17 00:54:51 +0000

    feat: create reusable input component

--- libs/ui-components/src/input-component/input-component.ts
+++ libs/ui-components/src/input-component/input-component.ts
@@ -1,14 +1,27 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  forwardRef,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'input-component',
   imports: [CommonModule],
   templateUrl: './input-component.html',
   styleUrl: './input-component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {
-
+export class InputComponent implements ControlValueAccessor {
   @Input() label: string = '';
   @Input() placeholder: string = '';
   @Input() type: string = 'text';
@@ -18,9 +31,31 @@ export class InputComponent {
 
   @Output() valueChange = new EventEmitter<string>();
 
+  private onChange = (_: any) => {};
+  private onTouched = () => {};
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
   onInput(event: Event) {
     const input = event.target as HTMLInputElement;
     this.value = input.value;
     this.valueChange.emit(this.value);
+
+    this.onChange(this.value);
+    this.onTouched();
   }
 }
