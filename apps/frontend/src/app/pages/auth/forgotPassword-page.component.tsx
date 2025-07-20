a9cdffae41f9b343ff3f6c1ecd5b71f2ef1505a9
McAdams R.D. <93725119+mcthekreator@users.noreply.github.com>
2025-07-20 01:11:07 +0000

    feat: created login page

---
+++ apps/frontend/src/app/pages/auth/forgotPassword-page.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ForgotPasswordLayout from '../../layout/forgotPassword.layout';
+
+const ForgotPasswordPage: React.FC = () => {
+  return (
+    <div>
+        <ForgotPasswordLayout/>
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
---
+++ apps/frontend/src/app/pages/auth/register-page.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegisterLayout from '../../layout/register.layout';
+
+const RegisterPage: React.FC = () => {
+  return (
+    <div>
+      <RegisterLayout />
+    </div>
+  );
+};
+
+export default RegisterPage;
