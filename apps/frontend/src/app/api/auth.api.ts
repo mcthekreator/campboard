14b7314befaa6113feb1aa43d344187322b3af6e
McAdams R.D. <93725119+mcthekreator@users.noreply.github.com>
2025-07-21 01:13:21 +0000

    chore: updated images

---
+++ apps/frontend/src/app/api/auth.api.ts
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import { AuthResponse } from '../types/auth..types';
+
+const API = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  withCredentials: true,
+});
+
+export const loginApi = async (
+  email: string,
+  password: string
+): Promise<AuthResponse> => {
+  const { data } = await API.post('/auth/login', { email, password });
+  return data;
+};
+
+
+
+export const registerApi = async (
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string
+): Promise<AuthResponse> => {
+  const { data } = await API.post('/auth/register', {
+    firstName,
+    lastName,
+    email,
+    password,
+  });
+  return data;
+};
+export const forgotPasswordApi = async (
+  email: string
+): Promise<AuthResponse> => {
+  const { data } = await API.post('/auth/forgot-password', { email });
+  return data;
+};
+
+export const resetPasswordApi = async (
+  email: string,
+  otp: string,
+  newPassword: string
+): Promise<AuthResponse> => {
+  const { data } = await API.post('/auth/reset-password', { email, otp, newPassword });
+  return data;
+};
+
+export const verifyOtpApi = async (
+  email: string,
+  otp: string
+): Promise<AuthResponse> => {
+  const { data } = await API.post('/auth/verify-user', { email, otp });
+  return data;
+};
