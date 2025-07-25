27ecc9d455db3f4b6287d7f2f2dab303406b5401
mcadamsx <mcadamsrd@gmail.com>
2025-07-25 04:56:40 +0000

    added new color variables

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -16,7 +16,10 @@ module.exports = {
         "purple-primary": "#4F46E5",
         "purple-secondary": "#D9D6FE",
         "text-primary": "#1A1A1A",
-        "white-secondary": "#c0bebe"
+        "text-secondary": "#374151",
+        "text-tertiary": "#6b7280",
+        "white-secondary": "#c0bebe",
+        "white-tertiary": "#e5e7eb"
       },
       animation: {
         grow: 'grow 0.8s ease-in-out infinite',
