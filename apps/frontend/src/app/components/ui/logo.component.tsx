b2ec16540fd91639dbb1d557ae12d9d08a45f1aa
mcadamsx <mcadamsrd@gmail.com>
2025-07-26 04:58:03 +0000

    created logo component

---
+++ apps/frontend/src/app/components/ui/logo.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogoComponent: React.FC = () => {
+  return (
+    <div className=" text-center ">
+      <img className="w-full  mx-auto" src="logo-dark-transparent.png" alt="" />
+    </div>
+  );
+};
+
+export default LogoComponent;
