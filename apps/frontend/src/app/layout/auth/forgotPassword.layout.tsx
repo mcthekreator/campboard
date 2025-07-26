9815527eb99cd623f9cb95e8fa9047eb28cdc763
mcadamsx <mcadamsrd@gmail.com>
2025-07-26 04:59:51 +0000

    updated forgot paassword

---
+++ apps/frontend/src/app/layout/auth/forgotPassword.layout.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import LeftAuthComponent from '../../components/features/left-auth.component';
+import AuthHeader from '../../components/ui/auth-header.component';
+import ForgotPasswordComponent from '../../components/features/forgot-password.component';
+
+const ForgotPasswordLayout: React.FC = () => {
+  return (
+    <div className="flex w-full min-h-screen">
+  <div className="w-[50%] hidden md:block">
+    <LeftAuthComponent />
+  </div>
+
+  <div className="flex w-full md:w-[60%] items-center">
+    <div className="w-10/12 mx-auto md:w-8/12">
+      <AuthHeader title='Login into your account' />
+      <ForgotPasswordComponent />
+    </div>
+  </div>
+</div>
+
+  );
+};
+
+export default ForgotPasswordLayout;
---
+++ apps/frontend/src/app/layout/auth/resetPassword-layout.component.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import LeftAuthComponent from '../../components/features/left-auth.component';
+import ResetPasswordComponent from '../../components/features/reset-password.component';
+
+const ResetPasswordLayout: React.FC = () => {
+  return (
+    <div className="flex w-full ">
+      <div className="w-[50%]  hidden md:block">
+        <LeftAuthComponent />
+      </div>
+      <div className="w-full md:w-[60%]">
+        <div className="w-10/12 mx-auto my-auto md:w-8/12">
+          <div className="text-center my-20">
+            <h1 className="my-4">Reset Password</h1>
+            <p>Please enter your detials to reset</p>
+          </div>
+          <ResetPasswordComponent />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPasswordLayout;
