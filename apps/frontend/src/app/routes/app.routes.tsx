51859c6b396c589b3ade36b64b48f5717028ce8c
mcadamsx <mcadamsrd@gmail.com>
2025-07-27 05:03:11 +0000

    added customer route

--- apps/frontend/src/app/routes/app.routes.tsx
+++ apps/frontend/src/app/routes/app.routes.tsx
@@ -1,7 +1,19 @@
 import { useRoutes } from 'react-router-dom';
 import AuthRoutes from './auth.route';
+import CustomerRoutes from './customer.route';
+import UnauthorizedPage from '../pages/unauthorized-page.component';
+
 const AppRoutes = () => {
-  return useRoutes([...AuthRoutes,]);
+  const unauthorizedRoute = {
+    path: "/unauthorized",
+    element: <UnauthorizedPage />,
+  };
+
+  return useRoutes([
+    ...AuthRoutes,
+    ...CustomerRoutes,
+    unauthorizedRoute
+  ]);
 };
 
 export default AppRoutes;
