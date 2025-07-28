b31ecd274ddecc7a21e592ac928add4f171cfca9
mcadamsx <mcadamsrd@gmail.com>
2025-07-28 05:47:16 +0000

    refactor serach component

--- apps/frontend/src/app/components/features/search.component.tsx
+++ apps/frontend/src/app/components/features/search.component.tsx
@@ -1,34 +1,14 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
 import { FiSearch } from 'react-icons/fi';
-import { useAuthStore } from '../../store/auth.store';
 
 const SearchComponent: React.FC = () => {
-  const setLoading = useAuthStore((state) => state.setLoading);
   const [email, setEmail] = useState('');
-  const navigate = useNavigate();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setLoading(true);
-
-    try {
-      await new Promise((resolve) => setTimeout(resolve, 3000));
-      toast.success('Login successful!');
-      navigate('/dashboard');
-    } catch (error: any) {
-      const errorMessage =
-        error?.response?.data?.message || error?.message || 'Login failed. Please try again.';
-      toast.error(errorMessage);
-    } finally {
-      setLoading(false);
-    }
-  };
+ 
 
   return (
     <div className="w-5/6 mx-auto hidden md:block">
-      <form onSubmit={handleSubmit} className=" mx-auto my-10">
+      <form  className=" mx-auto my-10">
         <div className="relative">
           <FiSearch className="absolute left-4 top-1/2 transform -translate-y-1/2 text-text-primary w-5 h-5" />
           <input
