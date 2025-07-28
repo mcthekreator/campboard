c17fa614667977c253aa67e4506becd4e911f56b
mcadamsx <mcadamsrd@gmail.com>
2025-07-28 11:06:56 +0000

    fix: fixed build fail

--- apps/frontend/src/app/api/auth.api.ts
+++ apps/frontend/src/app/api/auth.api.ts
@@ -4,6 +4,10 @@ import { AuthResponse } from '../types/auth..types';
 const API = axios.create({
   baseURL: 'https://ab63376c1341.ngrok-free.app/api',
   withCredentials: true,
+  headers: {
+    'ngrok-skip-browser-warning': 'true', // This is important for ngrok
+    'Content-Type': 'application/json',
+  },
 });
 
 export const loginApi = async (
