765bd17709411e7da060d0130a749d0a3b193ccf
mcadamsx <mcadamsrd@gmail.com>
2025-07-30 04:39:24 +0000

    feat: responasive auth

--- apps/frontend/src/app/layout/auth/login.layout.tsx
+++ apps/frontend/src/app/layout/auth/login.layout.tsx
@@ -5,7 +5,7 @@ import AuthHeader from '../../components/ui/auth-header.component';
 
 const LoginLayout: React.FC = () => {
   return (
-    <div className="flex flex-col md:flex-row w-full min-h-screen">
+    <div className="flex flex-col md:flex-row w-full">
       {/* Left Side (hidden on small screens) */}
       <div className="hidden md:block md:w-1/2">
         <LeftAuthComponent />
